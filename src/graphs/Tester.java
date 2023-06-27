8656d6e27c1cdb523d6135dcf62415f2eb4e1e15
Vishwanath <92073279+Vishwanath-oneorigin@users.noreply.github.com>
2023-06-27 23:04:01 +0530

    Update Tester.java
    Added 3 more syso

--- src/graphs/Tester.java
+++ src/graphs/Tester.java
@@ -5,5 +5,9 @@ public static void main(String[] args) {
 	
 	
 	System.out.println("Test here");
+	System.out.println("Test here222");
+	System.out.println("Test here");
+	System.out.println("Test here44");
+	
 }
 }
