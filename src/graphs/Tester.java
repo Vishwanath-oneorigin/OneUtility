e1b32964b09731bcbd4a0a3605e2fa4ea836497d
Vishwanath__K <vishwanath@oneorigin.us>
2023-06-27 23:06:47 +0530

    removed sysos

--- src/graphs/Tester.java
+++ src/graphs/Tester.java
@@ -3,11 +3,7 @@ package graphs;
 public class Tester {
 public static void main(String[] args) {
 	
-	
-	System.out.println("Test here");
-	System.out.println("Test here222");
-	System.out.println("Test here");
-	System.out.println("Test here44");
+
 	
 }
 }
